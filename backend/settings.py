cb7a43b363547d75889f7e6557ffaf3b20410bf7
owaismfos <owaismfos@gmail.com>
2025-09-13 13:50:05 +0530

    how frontend origin

--- backend/settings.py
+++ backend/settings.py
@@ -55,6 +55,26 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000"
 ]
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "HTTP_X_CLOUDFLARE_TOKEN",
+]
 
 CELERY_IMPORTS = ("main.utils.tasks",)
 
